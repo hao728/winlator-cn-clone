0cd5387ab5815ba3b0d159c2bc486264f2f2972c
hao728 <wkai42822@gmail.com>
2026-09-16 04:31:54 +0800

    Import IOException in MainApplication
    Added IOException import for file handling.

--- app/src/main/java/com/winlator/MainApplication.java
+++ app/src/main/java/com/winlator/MainApplication.java
@@ -19,6 +19,7 @@ import com.winlator.core.PatchUtils;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
